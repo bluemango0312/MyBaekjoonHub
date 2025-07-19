c943c4a53991c1a05503a510b12719391c0d62d9
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-07-19 18:54:28 +0900

    [Silver III] Title: 주유소, Time: 900 ms, Memory: 169808 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+// 1km = 1리터
+// 1 도시 = 1 주유소
+
+// 입력: 도시 개수 // 도로의 길이 // 주유소의 리터당 가격
+// 출력: 최소 비용
+
+// 1. 입력받기 - 각각 배열
+
+// 2. 도시 배열을 돌면서 계산하기
+
+// 3. 출력
+
+public class Main {
+	public static void main(String[] args) {
+		// 1. 입력받기 - 각각 배열
+		Scanner sc = new Scanner(System.in);
+
+		int cityCnt = sc.nextInt();
+		int[] cityCost = new int[cityCnt];
+		int[] loadLength = new int[cityCnt - 1];
+		
+		for (int i = 0; i < cityCnt - 1; i++) {
+			loadLength[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < cityCnt; i++) {
+			cityCost[i] = sc.nextInt();
+		}
+
+		// 2. 도시 배열을 돌면서 계산하기
+		int currentIndex = 0;
+		int nextIndex = 1;
+		int loadIndex = 1;
+		int resultCost = cityCost[0] * loadLength[0];
+
+		while (loadIndex < cityCnt - 1 && nextIndex < cityCnt && currentIndex < cityCnt) {
+			if (cityCost[currentIndex] <= cityCost[nextIndex]) {
+				resultCost += cityCost[currentIndex] * loadLength[loadIndex];
+				nextIndex++;
+				loadIndex++;
+			} else {
+				resultCost += cityCost[nextIndex] * loadLength[loadIndex];
+				currentIndex += nextIndex;
+				nextIndex++;
+				loadIndex++;
+			}
+		}
+
+		System.out.println(resultCost);
+	}
+
+}
