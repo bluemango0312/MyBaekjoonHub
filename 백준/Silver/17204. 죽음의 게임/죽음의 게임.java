8eea86d31c66a1c7e519f6170e0651b33af4635a
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-08 00:47:39 +0900

    [Silver III] Title: 죽음의 게임, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		List<Integer> game = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			game.add(Integer.parseInt(br.readLine()));
+		}
+
+		int result = 0;
+		int curIndex = 0;
+
+		while (curIndex != M) {
+			if (result > N) {
+				result = -1;
+				break;
+			}
+			result++;
+			curIndex = game.get(curIndex);
+		}
+
+		System.out.println(result);
+	}
+
+}
