275c318e18ba67b82b417f7b252bf697201ecb9b
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-11 00:19:50 +0900

    [Silver V] Title: 영화감독 숌, Time: 364 ms, Memory: 142076 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		String num = "0";
+
+		while (cnt < N) {
+			num = String.valueOf(Integer.parseInt(num) + 1);
+
+			if (num.contains("666")) {
+				cnt++;
+			}
+		}
+
+		System.out.println(num);
+	}
+
+}
