29218f0722ca491c10c502c507c7bedb9a64d588
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-11 19:04:50 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String str = br.readLine();
+		int result = 0;
+		int idx = 0;
+
+		while (idx < input.length() - str.length() + 1) {
+			if (input.substring(idx, idx + str.length()).equals(str)) {
+				result++;
+				idx += str.length();
+			} else {
+				idx++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
