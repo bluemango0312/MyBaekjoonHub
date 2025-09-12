22ee68c55fbe0a1406f9e9fc6c1e14e2497782d1
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-13 01:05:12 +0900

    [Silver V] Title: 피카츄, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/14405. 피카츄/피카츄.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		String[] canSpeackArray = { "pi", "ka", "chu" };
+		String result = "YES";
+
+		for (int i = 0; i < canSpeackArray.length; i++) {
+			S = S.replaceAll(canSpeackArray[i], " ");
+		}
+
+		for (int c : S.toCharArray()) {
+			if (c != ' ') {
+				result = "NO";
+				break;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
