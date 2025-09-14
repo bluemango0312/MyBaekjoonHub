361c45dd17a2adca53b6ecca9d9aaa27aaea376e
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-15 01:11:29 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder result = new StringBuilder();
+		String[] str = new String[N];
+
+		for (int i = 0; i < N; i++) {
+			str[i] = br.readLine();
+		}
+
+		for (int i = 0; i < str[0].length(); i++) {
+			boolean isSame = true;
+
+			for (int j = 0; j < str.length - 1; j++) {
+				if (str[j].charAt(i) != str[j + 1].charAt(i)) {
+					isSame = false;
+					break;
+				}
+			}
+
+			if (isSame == true && str[0].charAt(i) == '.') {
+				result.append(".");
+			} else if (isSame == true && str[0].charAt(i) != '.') {
+				result.append(str[0].charAt(i));
+			} else {
+				result.append("?");
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
