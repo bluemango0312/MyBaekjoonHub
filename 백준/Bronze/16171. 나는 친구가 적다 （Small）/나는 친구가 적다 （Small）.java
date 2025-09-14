a81965a1749dad1076209adbf958814adcb0e8b3
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-15 06:48:16 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String findWord = br.readLine();
+		boolean result = true;
+
+		// 소문자로 통일
+		input = input.toLowerCase();
+		findWord = findWord.toLowerCase();
+
+		// 숫자 제거
+		int i = 0;
+		while (i < input.length()) {
+			char c = input.charAt(i);
+
+			if (Character.isDigit(c)) {
+				input = input.replace(String.valueOf(c), "");
+			} else {
+				i++;
+			}
+		}
+
+		// input에 찾는 문자열이 있는지 확인
+		result = input.contains(findWord);
+
+		if (result == true) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+
+}
