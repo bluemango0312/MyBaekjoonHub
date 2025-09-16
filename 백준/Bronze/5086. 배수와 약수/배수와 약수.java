6911dd1802e2d6062341abd0b66afca598a4f0cb
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-16 18:41:18 +0900

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+
+		while (a != 0 && b != 0) {
+			if (isFactor(a, b)) {
+				sb.append("factor").append("\n");
+			} else if (isMultiple(a, b)) {
+				sb.append("multiple").append("\n");
+			} else {
+				sb.append("neither").append("\n");
+			}
+
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(sb);
+	}
+
+	private static boolean isMultiple(int a, int b) {
+		if (a % b == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private static boolean isFactor(int a, int b) {
+		if (b % a == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
