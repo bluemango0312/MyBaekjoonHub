e6122a640d064e3e8b8605d25b58ef948b98db1a
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-17 18:01:08 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] divisor = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			divisor[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 정렬
+		Arrays.sort(divisor);
+
+		// 첫 수와 마지막 수 곱하기
+		System.out.println(divisor[0] * divisor[N - 1]);
+	}
+}
