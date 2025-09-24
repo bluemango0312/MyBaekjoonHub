8546b3bf327bcd717998e34f7461e32612eca3e5
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-09-24 18:51:19 +0900

    [Silver II] Title: 나무 자르기, Time: 952 ms, Memory: 119544 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] treeHeight = new int[N];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			treeHeight[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(treeHeight);
+
+		// binary search
+		int left = 0;
+		int right = treeHeight[N - 1];
+		int ans = 0;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			long cal = 0;
+
+			for (int i = N - 1; i >= 0; i--) {
+				if (treeHeight[i] > mid) {
+					cal += treeHeight[i] - mid;
+				}
+				if (cal >= M)
+					break;
+			}
+
+			if (cal >= M) {
+				ans = mid;
+				left = mid + 1;
+			} else if (cal < M) {
+				right = mid - 1;
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+}
