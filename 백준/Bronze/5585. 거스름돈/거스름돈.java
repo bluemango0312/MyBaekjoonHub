d05d19cc510ffd6b969950b1e8463af681fb6592
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-10-14 15:18:08 +0900

    [Bronze II] Title: 거스름돈, Time: 96 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int result;
+	static int change;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine()); // 타로가 지불할 돈
+		change = 1000 - input;
+		result = 0;
+
+		getChange(500);
+		getChange(100);
+		getChange(50);
+		getChange(10);
+		getChange(5);
+		getChange(1);
+		
+		System.out.println(result);
+	}
+
+	private static void getChange(int num) {
+		if (change >= num) {
+			result += change / num;
+			change %= num;
+		}
+	}
+
+}
