3c69a3db35a768033500dab101483e17f50b9e2b
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-10-22 10:12:46 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 21992 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		// 파스칼의 삼각형 점화식 사용
+		long[][] dp = new long[N + 1][K + 1];
+		dp[0][0] = 1;
+
+		for (int i = 1; i <= N; i++) {
+			// 기본 조건
+			dp[i][0] = 1;
+
+			// 점화식
+			for (int j = 1; j <= K; j++) {
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+			}
+		}
+
+		System.out.println(dp[N][K]);
+	}
+
+}
