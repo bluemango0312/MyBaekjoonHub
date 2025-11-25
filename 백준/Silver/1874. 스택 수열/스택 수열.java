cfbeb4d34f124cb5fe7c9eb80a59c937a12a98a2
서정후 <96233329+bluemango0312@users.noreply.github.com>
2025-11-25 21:44:55 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 26184 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] input = new int[N];
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+
+		int inputCnt = 0;
+		int curNum = input[inputCnt];
+		if (inputCnt < N - 1) {
+			inputCnt++;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (stack.isEmpty() || curNum > stack.peek()) {
+				stack.add(i);
+				sb.append("+").append("\n");
+			}
+
+			while (!stack.isEmpty() && curNum == stack.peek()) {
+				stack.pop();
+				curNum = input[inputCnt];
+
+				if (inputCnt < N - 1) {
+					inputCnt++;
+				}
+
+				sb.append("-").append("\n");
+			}
+
+//			System.out.println(stack);
+		}
+
+		if (curNum == input[N - 1]) {
+			System.out.println(sb);
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
